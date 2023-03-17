e99cf6d9c435898191554a2f4f183c28feb74ce3
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-17 09:28:04 -0700

    my work

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,31 +9,22 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int n;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 100; j++)
 		{
-			for (k = 0; k < 10; k++)
+			if (i < j)
 			{
-
-				for (n = 0; n < 10; n++)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					if (k != 0 || n != 0)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(n + '0');
-					}
-					if ((i != 9 && j != 8) || (k != 0 || n != 0))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
