7ecf571e1faacf8c2c716c48a9c4c2b2f3a0c5b3
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-17 06:22:05 -0700

    my work

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,9 +14,14 @@ int main(void)
 	{
 		if (i == 9)
 			putchar(i + '0');
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+
+
 	}
 	return (0);
 }
