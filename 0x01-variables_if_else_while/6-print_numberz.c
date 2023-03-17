872d15c0621f0d9a1d79055a665a43069c212e93
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-17 05:26:03 -0700

    my work

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main _ is my main function
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
