d217187ececcd88d4e2b4c4b71a4dd28da1ec1b0
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-17 05:47:51 -0700

    my work

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - is my main function
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
