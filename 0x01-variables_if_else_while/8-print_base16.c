acfd147d8422a62690ea01d4bb19f74797bf37d1
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-17 06:00:33 -0700

    my work

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	int i;
+	char j;
 
-	for (i = 0; i < 16; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
+	}
+	for (j = 'a'; j < 'f'; j++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
