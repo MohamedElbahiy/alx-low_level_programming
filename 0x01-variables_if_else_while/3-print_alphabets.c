644bca4920e6614efa57438d6f7e674dd8ebb249
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-17 04:53:14 -0700

    my work

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - is my main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+	putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
