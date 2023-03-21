424f1aea60a1b793a8722b060d04b8edb7a6a47c
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 09:56:52 -0700

    my work

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main -my main
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	long i = 0;
+	long j = 1;
+	long k;
+	int x;
+
+	for (x = 0; x < 98; x++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		if (k == 1)
+		{
+			printf("%ld", k);
+		}
+		else
+		{
+			printf(", %ld", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
