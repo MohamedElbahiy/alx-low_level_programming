70f1b64e464d48fd6e556f63f8e6bd1af9c6d4cd
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 05:18:13 -0700

    my work

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - my main
+ *
+ * @n: parameter
+ *
+ * Return: return no thing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+	{
+		if (i > 9 || i < -9)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
