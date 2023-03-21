b0a2ef60e47af244d133c135dd1c96690f8b56ea
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 08:55:36 -0700

    my work

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - my main
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+	int k;
+	int x;
+
+	for (x = 0; x < 50; x++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		if (k == 1)
+		{
+			printf("k");
+		}
+		else
+		{
+			printf(", %d", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
