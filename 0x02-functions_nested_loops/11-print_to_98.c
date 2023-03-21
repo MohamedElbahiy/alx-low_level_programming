9e1554756ee4fa512d6db2fad12bb3f3a4838013
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 05:35:43 -0700

    my work

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,46 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 98; i++)
+	if (n < 98)
 	{
-		if (i > 9 || i < -9)
+		for (i = n; i < 98; i++)
 		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (i > 9 || i < -9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
 		}
-		else
-		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
+		_putchar('9');
+		_putchar('8');
+		_putchar('\n');
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
