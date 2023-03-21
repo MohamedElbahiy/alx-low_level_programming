475662330c2b27a54ebd4c65baa182bbe48778de
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 00:15:19 -0700

    my work

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print is my main
+ *
+ * Return: not return
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(i);
+}
