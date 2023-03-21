5139afcebc0f3e93ba2b3bd1019724cb65ed1875
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 09:46:11 -0700

    my work

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,13 +9,18 @@ int main(void)
 	long i = 0;
 	long j = 1;
 	long k = 0;
+	long x = 0;
 
-	while (k < 4000000)
+	while (x < 4000000)
 	{
 		k = i + j;
 		i = j;
 		j = k;
+		if (k % 2 == 0)
+		{
+			x = x + k;
+		}
 	}
-	printf("%ld\n", j);
+	printf("%ld\n", x);
 	return (0);
 }
