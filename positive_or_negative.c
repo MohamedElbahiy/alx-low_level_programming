37922cf370d4460b620d3c3a09440a5f0f40dfa7
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-21 23:39:28 -0700

    my work

---
+++ positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * positive_or_negative - my main
+ *
+ * @i: parameter
+ *
+ * Return: return nothing
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
+}
