8712585bd7938c4b89af009407f7c910db861230
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-23 07:18:24 -0700

    my wok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include "main.h"
-<<<<<<< HEAD
+#include <stdio.h>
 /**
  * main - my main
  *
@@ -7,39 +7,39 @@
  */
 int main(void)
 {
-	int i;
-	for (i = 1; i <= 100; i++)
-	{
-	
-		if (i != 1)
-		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-			{
-				printf(" FizzBuzz");
-			}
-			else if (i % 3 == 0)
-			{
-				printf(" Fizz");
-			}
-			else if (i % 5 == 0)
-			{
-				printf(" Buzz");
-			}
-			else
-			{
-				printf(" %d", i);
-			}
-		}
-		else
-		{
-			printf(" %d", i);
-		}
+        int i;
+        for (i = 1; i <= 100; i++)
+        {
 
-	}
-	printf("\n");
-	return (0);
-=======
+                if (i != 1)
+                {
+                        if ((i % 3 == 0) && (i % 5 == 0))
+                        {
+                                printf(" FizzBuzz");
+                        }
+                        else if (i % 3 == 0)
+                        {
+                                printf(" Fizz");
+                        }
+                        else if (i % 5 == 0)
+                        {
+                                printf(" Buzz");
+                        }
+                        else
+                        {
+                                printf(" %d", i);
+                        }
+                }
+                else
+                {
+                        printf("%d", i);
+                }
 
+        }
+        printf("\n");
+        return (0);
+}
+/**
 int main(void)
 {
   for (i = 1; i <= 100; i++)
@@ -55,19 +55,4 @@ int main(void)
         printf(" Fizz");
       }
       else if (i % 5 == 0)
-      {
-        printf(" Buzz");
-      }
-      else
-      {
-        printf(" %d", i);
-      }
-    }
-    else
-    {
-      printf(" %d", i);
-    }
-  }
-  printf("\n");
->>>>>>> 359c69e3a37128795d920a0ab75c59a2310394f5
-}
+      */
