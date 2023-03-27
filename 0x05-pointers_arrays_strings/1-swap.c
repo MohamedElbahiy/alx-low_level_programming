4c52ac6bb1243180f9b373d00d336d169aba2881
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-27 05:11:30 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - my main
+ *
+ * @a: parameter
+ *
+ * @b: parameter
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
