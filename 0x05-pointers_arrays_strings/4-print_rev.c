ff0a323def33a62dc3a4cbccce6d95149743d667
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-27 07:22:32 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - my main
+ *
+ * @s: parameter
+ *
+ * Return: return nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j++;
+	}
+	for (i = j; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
