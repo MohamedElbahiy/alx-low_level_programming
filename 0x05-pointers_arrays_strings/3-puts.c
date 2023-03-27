8f8fb4ef22f09b02461d5ffff53521fe8ff5575d
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-27 06:52:30 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - my main
+ *
+ * @str: parameter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		
+	}
+	_putchar('\n');
+}
