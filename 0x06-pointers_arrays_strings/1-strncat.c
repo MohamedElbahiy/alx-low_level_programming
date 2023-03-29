7c941917c2350ccb26780e91223b85bf86d33dd8
MohamedElbahiy <asmsoa22@gmail.com>
2023-03-29 08:25:51 -0700

    my work

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,29 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int dl = 0;
+	int sl = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		dl++;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[dl + i] = src[i];
+		sl++;
+	}
+	if (n <= sl)
+	{
+		for (i = 0; i < n; i++)
+		{
+			dest[dl + i] = src[i];
+		}
+	}
+	else
+	{
+		for (i = 0; i < sl; i++)
+		{
+			dest[dl + i] = src[i];
+		}
 	}
 	return (dest);
 }
